40bc21a822e1f9ca22a6cdb11e6d46635a22c66a
Ника_комп <v.matuk@skyeng.ru>
2022-06-16 20:49:02 +0300

    добавила модели

--- ads/models.py
+++ ads/models.py
@@ -10,3 +10,17 @@ class Ads(models.Model):
 
 class Categories(models.Model):
     name = models.CharField(max_length=100)
+
+class Location(models.Model):
+    name = models.CharField(max_length=1000)
+    lat = models.FloatField()
+    lng = models.FloatField()
+
+class User(models.Model):
+    first_name = models.CharField(max_length=1000)
+    last_name = models.CharField(max_length=10)
+    username = models.CharField(max_length=10)
+    password = models.CharField(max_length=10)
+    role = models.CharField(max_length=10)
+    age = models.BigIntegerField()
+    location_id = models.BigIntegerField()
